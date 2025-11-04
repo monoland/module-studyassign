711787f1096c5b60ad03356357749d2dc3e35fff
sudarmono <30992530+monoland@users.noreply.github.com>
2025-11-04 08:25:52 +0700

    update

---
+++ resources/views/reports/recommend.blade.php
@@ -0,0 +1,99 @@
+<table>
+    <tr>
+        <td colspan="3">REKOMENDASI MELANJUTKAN PENDIDIKAN</td>
+    </tr>    
+    <tr>
+        <td colspan="3">Nomor : {{ $recommend_letter_number }}</td>
+    </tr>
+    
+    <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>a.</td>
+        <td>Dasar</td>
+        <td>:</td>
+        <td>Peraturan Gubernur Banten Nomor 32 Tahun 2020 Tentang Pengembangan Kompetensi Aparatur Sipil Negara melalui Pemberian Tugas Belajar, Izin Belajar, dan Magang/Praktek Kerja di Lingkungan Pemerintah Provinsi Banten.</td>
+    </tr>
+
+    <tr>
+        <td>b.</td>
+        <td>Menimbang</td>
+        <td>:</td>
+        <td>Surat {{ $recommend_letter_position }} tentang Izin Melanjutkan Pendidikan a.n. {{ $name }} Nomor : {{ $recommend_permission_number }} tanggal : {{ $recommend_permission_date }}</td>
+    </tr>
+
+    <tr>
+        <td colspan="3">Kepala Badan Kepegawaian Daerah memberikan rekomendasi kepada :</td>
+    </tr>
+
+    <tr>
+        <td>a.</td>
+        <td>Nama</td>
+        <td>:</td>
+        <td>{{ $name }}</td>
+    </tr>
+
+    <tr>
+        <td>b.</td>
+        <td>NIP</td>
+        <td>:</td>
+        <td>{{ $biodata_id }}</td>
+    </tr>
+
+    <tr>
+        <td>c.</td>
+        <td>Jabatan</td>
+        <td>:</td>
+        <td>{{ $position_name }}</td>
+    </tr>
+
+    <tr>
+        <td>d.</td>
+        <td>Pangkat/Gol</td>
+        <td>:</td>
+        <td>{{ $section_name }}</td>
+    </tr>
+
+    <tr>
+        <td>e.</td>
+        <td>Unit Kerja</td>
+        <td>:</td>
+        <td>{{ $section_name }}</td>
+    </tr>
+
+    <tr>
+        <td>f.</td>
+        <td>Pendidikan Terakhir</td>
+        <td>:</td>
+        <td>{{ $section_name }}</td>
+    </tr>
+
+    <tr>
+        <td colspan="3">
+            Untuk melanjutkan pendidikan jenjang {{ $target_edulevel_name }} pada perguruan tinggi {{ $college_name }} program studi {{ $study_program }} dengan tidak menganggu tugas kedinasan.<br/>
+            Demikian rekomendasi ini dibuat untuk dipergunakan seperlunya.
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
+
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>
+            Serang, {{ $recommend_letter_date }}<br/>
+            a.n. Kepala Badan Kepegawaian Daerah<br/>
+            a.n. Kepala Bidang Pengembangan ASN<br/>
+            <br/>
+            <br/>
+            <br/>
+            {{ $recommend_letter_officer }}<br/>
+            {{ $recommend_letter_section }}<br/>
+            NIP. {{ $recommend_letter_nip }}<br/>
+        </td>
+    </tr>
+</table>
